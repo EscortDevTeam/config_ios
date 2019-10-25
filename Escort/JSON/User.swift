2b6bdd7186e474b302d84ac0622e9257d76903b2
FirstZverev <46249639+FirstZverev@users.noreply.github.com>
2019-10-25 09:19:57 +0300

    Revert "Comment you want"
    This reverts commit 787f6667267d98ae2021758d901b70f75510db5c.

--- Escort/JSON/User.swift
+++
@@ -1,9 +0,0 @@
-//
-//  User.swift
-//  Escort
-//
-//  Created by Pavel Vladimiroff on 13/08/2019.
-//  Copyright © 2019 pavit.design. All rights reserved.
-//
-
-
--- README.md
+++
@@ -1,2 +0,0 @@
-# escort
-#Information next
